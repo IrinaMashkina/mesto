26216518ea7f673e2071a5be62a8ca0a60841b7d
Irina Mashkina <irinamashkina@list.ru>
2021-01-25 12:28:14 +0500

    feat: добавить обработчик submit

--- script/script.js
+++ script/script.js
@@ -1,26 +1,41 @@
 let formElement = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close');
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__job");
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__profession');
 
 
 editButton.addEventListener('click', function() {
     formElement.classList.add('popup_opened');
+    nameInput = profileName;
+});
+
+popupClose.addEventListener('click', function() {
+    formElement.classList.remove('popup_opened');
+        
 });
 
 
-// let nameInput = formElement.querySelector(".popup__name");
-// let jobInput = formElement.querySelector(".popup__job");
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    let nameInput = formElement.querySelector(".popup__name");
+    let nameInputValue = nameInput.value;
+    
+    let jobInput = formElement.querySelector(".popup__job");
+    let jobInputValue = jobInput.value;
+    
+    console.log(nameInputValue);
+    console.log(jobInputValue);
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__profession');
 
+    profileName.textContent = nameInputValue;
+    profileJob.textContent = jobInputValue;
+    
+    formElement.classList.remove('popup_opened');
 
-// function formSubmitHandler (evt) 
-//     evt.preventDefault(); 
+}
 
-//     // Получите значение полей jobInput и nameInput из свойства value
-
-//     // Выберите элементы, куда должны быть вставлены значения полей
-
-//     // Вставьте новые значения с помощью textContent
-
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler); 
