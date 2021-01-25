82e74314370ab784be7873e4151580be10446636
Irina Mashkina <irinamashkina@list.ru>
2021-01-25 20:09:18 +0500

    feat:кнопка закрытия попапа без изменения inputов

--- script/script.js
+++ script/script.js
@@ -1,21 +1,28 @@
 let formElement = document.querySelector('.popup');
+
 let editButton = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close');
+
 let nameInput = formElement.querySelector(".popup__name");
 let jobInput = formElement.querySelector(".popup__job");
+
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__profession');
+
+let nameInputValue = nameInput.value;
+let jobInputValue = jobInput.value;
 
 
 editButton.addEventListener('click', function() {
     formElement.classList.add('popup_opened');
-    nameInput = profileName;
-});
+    nameInput.value=profileName.textContent;
+    jobInput.value=profileJob.textContent;
+    });    
+    
 
 popupClose.addEventListener('click', function() {
-    formElement.classList.remove('popup_opened');
-        
-});
+    formElement.classList.remove('popup_opened');      
+    });
 
 
 function formSubmitHandler (evt) {
@@ -26,8 +33,6 @@ function formSubmitHandler (evt) {
     let jobInput = formElement.querySelector(".popup__job");
     let jobInputValue = jobInput.value;
     
-    console.log(nameInputValue);
-    console.log(jobInputValue);
     let profileName = document.querySelector('.profile__name');
     let profileJob = document.querySelector('.profile__profession');
 
